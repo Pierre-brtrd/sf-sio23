fe9b0e102d9dee2c5b319070935a3eb082f2631a
pierre.bertrand.professionnel <pierre.bertrand.professionnel@gmail.com>
2023-02-15 13:10:46 +0100

    feat: finish vich uploader config

--- src/Form/ArticleType.php
+++ src/Form/ArticleType.php
@@ -5,6 +5,7 @@ namespace App\Form;
 use App\Entity\Article;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
+use Vich\UploaderBundle\Form\Type\VichImageType;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\Extension\Core\Type\TextareaType;
@@ -27,6 +28,15 @@ class ArticleType extends AbstractType
                 'attr' => [
                     'placeholder' => 'Contenu de votre article'
                 ]
+            ])
+            ->add('imageFile', VichImageType::class, [
+                'required' => false,
+                'download_uri' => false,
+                'image_uri' => true,
+                'asset_helper' => true,
+                'label' => 'Image',
+                'allow_delete' => true,
+                'delete_label' => 'Supprimer l\'image',
             ]);
     }
 
