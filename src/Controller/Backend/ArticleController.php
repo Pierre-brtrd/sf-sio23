028d530b8045100f00af8a98604ff1fbd2313d9b
pierre.bertrand.professionnel <pierre.bertrand.professionnel@gmail.com>
2023-02-15 09:18:33 +0100

    feat: add crud for articles

--- src/Controller/Backend/ArticleController.php
+++ src/Controller/Backend/ArticleController.php
@@ -8,6 +8,7 @@ use App\Repository\ArticleRepository;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\RedirectResponse;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 #[Route('/admin/article')]
@@ -27,7 +28,7 @@ class ArticleController extends AbstractController
     }
 
     #[Route('/create', name: 'admin.article.create', methods: ['GET', 'POST'])]
-    public function create(Request $request): Response
+    public function create(Request $request): Response|RedirectResponse
     {
         $article = new Article();
 
@@ -45,5 +46,45 @@ class ArticleController extends AbstractController
         return $this->render('Backend/Article/create.html.twig', [
             'form' => $form,
         ]);
+    }
+
+    #[Route('/update/{id}', name: 'admin.article.update', methods: ['GET', 'POST'])]
+    public function update(Article $article, Request $request): Response|RedirectResponse
+    {
+        $form = $this->createForm(ArticleType::class, $article);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $this->repo->save($article, true);
+            $this->addFlash('success', 'Article mis à jour avec succès');
+
+            return $this->redirectToRoute('admin.article.index');
+        }
+
+        return $this->render('Backend/Article/update.html.twig', [
+            'form' => $form,
+            'article' => $article,
+        ]);
+    }
+
+    #[Route('/delete/{id}', name: 'admin.article.delete', methods: ['POST', 'DELETE'])]
+    public function delete(Article $article, Request $request): RedirectResponse
+    {
+        if (!$article instanceof Article) {
+            $this->addFlash('error', 'Article non trouvé');
+
+            return $this->redirectToRoute('admin.article.index');
+        }
+
+        if ($this->isCsrfTokenValid('delete' . $article->getId(), $request->get('_token'))) {
+            $this->repo->remove($article, true);
+            $this->addFlash('success', 'Article deleted successfully');
+
+            return $this->redirectToRoute('admin.article.index');
+        }
+
+        $this->addFlash('error', 'invalid token');
+
+        return $this->redirectToRoute('admin.article.index');
     }
 }
--- src/Form/ArticleType.php
+++ src/Form/ArticleType.php
@@ -7,7 +7,6 @@ use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
-use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 
 class ArticleType extends AbstractType
