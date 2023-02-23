7a6fbb043f3ca0b4ca25545a5b78047fe56b2941
pierre.bertrand.professionnel <pierre.bertrand.professionnel@gmail.com>
2023-02-23 09:34:24 +0100

    feat: add reset password feature

--- config/bundles.php
+++ config/bundles.php
@@ -17,4 +17,5 @@ return [
     Stof\DoctrineExtensionsBundle\StofDoctrineExtensionsBundle::class => ['all' => true],
     Vich\UploaderBundle\VichUploaderBundle::class => ['all' => true],
     Symfony\UX\Autocomplete\AutocompleteBundle::class => ['all' => true],
+    SymfonyCasts\Bundle\ResetPassword\SymfonyCastsResetPasswordBundle::class => ['all' => true],
 ];
